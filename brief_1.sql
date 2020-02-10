9ac33a9f389247272400f00754b0eeea7b270ed4
Jacques Garré <jcqs.gr@gmail.com>
2020-02-10 23:19:11 +0100

    ain't nobody got time fo' that

--- brief_1.sql
+++ brief_1.sql
@@ -64,6 +64,9 @@ LINES TERMINATED BY '\n' IGNORE 1 LINES
     @dummy
 );
 
+
+
+
 -- 1) combien de doublons avons-nous
 
 SELECT 
@@ -124,7 +127,11 @@ FROM
         HAVING COUNT(*) > 1
     ) T1;
 
+
+
+
 -- 2) supprimer les doublons
+
 CREATE TABLE tmp AS
 SELECT 
     * 
@@ -157,12 +164,20 @@ GROUP BY
 DROP TABLE client_0;
 RENAME TABLE tmp TO client_0;
 
+
+
+
 -- 3) combien de vendeurs étrangers avons-nous
+
 SELECT COUNT(*) AS nb_vendeurs_etrangers FROM client_0 WHERE SELLER_COUNTRY NOT LIKE "%FRANCE%";
+
+
+
 
 -- 4) creer la table vendeur1 composée des vendeurs étrangers,
 -- la table vendeur2 composée des vendeurs français 
 -- et la table vendeur3 composées des vendeurs français basés en métropole
+
 CREATE TABLE vendeur1 AS
 SELECT * FROM client_0 WHERE SELLER_COUNTRY NOT LIKE "%FRANCE%";
 CREATE TABLE vendeur2 AS
@@ -171,8 +186,11 @@ CREATE TABLE vendeur3 AS
 SELECT * FROM client_0 WHERE SELLER_COUNTRY LIKE "%METROPOLITAN%";
 
 
+
+
 -- 5) quelle est la probabilité pour un vendeur français d'avoir
 --  un bon score si la vente a lieu un lundi
+
 SELECT 
     T1.nb_vendeurs / (SELECT COUNT(*) AS nb_total FROM vendeur2) AS PROBABILITE
 FROM
@@ -206,8 +224,12 @@ FROM
         AND SELLER_SCORE_COUNT LIKE '%100000<1000000%'
     ) T1;
 
+
+
+
 -- 6) quel est le montant total des 
 -- articles vendus par famille de produits
+
 SELECT
     PRODUCT_FAMILY,
 	SUM(
@@ -220,8 +242,12 @@ SELECT
 FROM client_0
 GROUP BY PRODUCT_FAMILY;
 
+
+
+
 -- 7) Entre nationaux et étrangers 
 -- qui sont ceux qui ont le plus vendu d'articles?
+
 SELECT 
     CASE
         WHEN (SELECT COUNT(*) FROM vendeur2) > (SELECT COUNT(*) FROM vendeur1) THEN 'Vendeurs nationaux'
@@ -229,20 +255,32 @@ SELECT
         ELSE 'Vendeurs étrangers'
     END AS QUI_A_LE_PLUS_DE_VENTES;
 
+
+
+
 -- 8) Créer la table produit_1 (à partir de la table vendeur1) 
 -- comportant le montant des types de produits par famille de produits
+
 CREATE TABLE produit_1 
 SELECT PRODUCT_FAMILY, ITEM_PRICE
 FROM vendeur1 GROUP BY PRODUCT_FAMILY, ITEM_PRICE;
 
+
+
+
 -- 9) Créer la table produit_2 (à partir de la table vendeur2) 
 -- comportant le montant des types de produits par famille de produits
+
 CREATE TABLE produit_2 
 SELECT PRODUCT_FAMILY, ITEM_PRICE
 FROM vendeur2 GROUP BY PRODUCT_FAMILY, ITEM_PRICE;
 
+
+
+
 -- 10) Créer la table produits (à partir des tables vendeur1 & vendeur2) comportant 
 -- le montant des types de produits par famille de produits : pas de doublons svp
+
 CREATE TABLE produits
 SELECT PRODUCT_FAMILY,ITEM_PRICE
 FROM vendeur1 GROUP BY PRODUCT_FAMILY,ITEM_PRICE
@@ -250,27 +288,14 @@ UNION
 SELECT PRODUCT_FAMILY,ITEM_PRICE
 FROM vendeur2 GROUP BY PRODUCT_FAMILY,ITEM_PRICE;
 
+
+
+
 -- 11) En considérant que le deuxième achat effectué par un client constitue
 --  un complément d'achat et non un doublon, l'entreprise vous demande de créer
 --   à partir du fichier csv de départ une nouvelle table nommée vente_finale
 --en affichant pas les ventes complémentaires. 
 --    Toutefois les montants affectés à ces ventes doivent figurer dans la nouvelles table.
-
--- 109
-
--- 127
-
--- 67
-
--- 68
-
--- 94
-
-
-
-SELECT ID, CNT FROM (SELECT ID, COUNT(ID) AS CNT FROM client_0 GROUP BY ID) T1 WHERE CNT > 1;
-
-
 
 CREATE TABLE vente_finale3 AS
 SELECT
